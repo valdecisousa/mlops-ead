583bf3466482b6c3cc29616ce267bf071d3b31d8
Valdeci Sousa <valdeci.is@gmail.com>
2026-06-16 14:28:27 -0300

    organização do projeto MLOps

--- devops_mlops/locustfile.py
+++ devops_mlops/locustfile.py
@@ -2,15 +2,6 @@ from locust import HttpUser, task, between
 
 
 class ApiLoadRunner(HttpUser):
-    """
-    Sends a POST request to the '/predict' endpoint with a JSON payload.
-
-    Args:
-        self: The instance of the class.
-
-    Returns:
-        None
-    """
     wait_time = between(0.5, 2.5)
 
     @task
